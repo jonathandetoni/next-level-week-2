95f58bdc1e07cdd084209c043e161ebf71e53254
Jonathan de Toni <jonathan@tonidev.com.br>
2020-08-06 18:19:19 -0300

    feat: Inclusao da pagina de listagem dos professores.

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,8 +1,41 @@
 import React from 'react';
 
+//components
+import PageHeader from '../../components/PageHeader';
+import TeacherItem from '../../components/TeacherItem';
+
+//css
+import './style.css';
+
 function TeacherList(){
     return(
-        <h1>Teacher List</h1>
+        <div id="page-teacher-list" className="container">
+            <PageHeader title="Este são os proffys dísponiveis."> 
+                <form id="search-teachers">
+                    <div className="input-block">
+                        <label htmlFor="subject">Matéria</label>
+                        <input type="text" id="subject"/>
+                    </div>
+
+                    <div className="input-block">
+                        <label htmlFor="week_day">Dia da Semana</label>
+                        <input type="text" id="week_day"/>
+                    </div>
+
+                    <div className="input-block">
+                        <label htmlFor="time">Hora</label>
+                        <input type="text" id="time"/>
+                    </div>
+                </form>
+            </PageHeader>
+
+            <main>
+                <TeacherItem />
+                <TeacherItem />
+                <TeacherItem />
+                <TeacherItem />
+            </main>
+        </div>
     );
 }
 
